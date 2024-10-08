da606afed368c7a5ed1c52a8ba80fef9c28cf737
zt-ns <1529427084@qq.com>
2024-10-08 15:49:46 +0800

    123

---
+++ src/main/java/com/itheima/test.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author 曾涛
+ * @version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
