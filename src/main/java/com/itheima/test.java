ab7507333d4ac59591a59dcd46afe82ecbc77a2f
zt-ns <1529427084@qq.com>
2024-10-08 15:51:09 +0800

    12334

--- src/main/java/com/itheima/test.java
+++ src/main/java/com/itheima/test.java
@@ -7,5 +7,6 @@ package com.itheima;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
